a26612252560db69dec66609bf7018780a3664d0
HoJLter <hojlter.work@gmail.com>
2025-05-08 22:16:39 +0300

    Add: new config.py

--- src/dmddl/config/config.py
+++ src/dmddl/config/config.py
@@ -1,53 +1,28 @@
-from pathlib import Path
-import json
+from typing import Literal, Dict
+
+from openai import api_key
+from pydantic_settings import BaseSettings
+from pydantic import Field, field_validator
+from random import randint
+from dmddl.llm import OpenAI, Deepseek
 
 
-
-class UserConfig:
-    """Represents dmddl config object. Uses singleton pattern"""
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-
-        return cls.__instance
+ALLOWED_MODELS = ["Deepseek", "OpenAI"]
 
 
-    def __init__(self):
-        expected_conf_path = Path.home() / "dmddl.json"
-        # if not expected_conf_path.exists():
-        with open(expected_conf_path, "w") as file:
-            __conf_structure = {
-                "api-keys":{
-                    "OpenAI": "",
-                    "Deepseek": ""
-                },
-                "in_use_model": ""
-            }
-            json.dump(__conf_structure, file)
-        self.conf_path = expected_conf_path
+class ApiKeysDict(dict):
+    def __setitem__(self, key, value):
+        if key in ALLOWED_MODELS:
+            return super().__setitem__(key, value)
+        else:
+            raise ValueError("LLM Provider not found")
 
 
-    @property
-    def content(self) -> dict:
-        with open(self.conf_path, "r") as file:
-            content = json.load(file)
-            return content
+class LLMConfig(BaseSettings):
+    llm_in_use:str | None
+    api_keys: ApiKeysDict = Field(default_factory=ApiKeysDict)
 
 
-    @content.setter
-    def content(self, new_content):
-        for key in self.content:
-            print(key)
-        with open(self.conf_path, "w") as file:
-            json.dump(new_content, file, indent=4)
-
-
-
-
-
-
-config = UserConfig()
-config.content = {"test": "test"}
-print(config.content)
+config = LLMConfig(llm_in_use="Deepseek")
+config.api_keys['Deepseek'] = "sdwafczawdca"
+print(config.llm_in_use)
